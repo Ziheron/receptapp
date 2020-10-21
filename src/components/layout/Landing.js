38d94e59cd2e9bd2561a0ab0882a232b3fdf4c11
Reinholdsson <nbte@rocketmail.com>
2020-10-21 19:07:08 +0200

    added some navigation

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
   return (
@@ -7,15 +8,15 @@ const Landing = () => {
         <div className="landing-inner">
           <h1 className="x-large">Recept</h1>
           <p className="lead">
-          Varför inte bara skriva något roligt här?
+          What´s your favorite meal?
           </p>
           <div className="buttons">
-            <a href="register.html" className="btn btn-primary">
+            <Link to='register' className="btn btn-primary">
               Sign Up
-            </a>
-            <a href="login.html" className="btn btn-light">
+              </Link>
+            <Link to='login' className="btn btn-light">
               Login
-            </a>
+              </Link>
           </div>
         </div>
       </div>
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,15 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <nav className="navbar bg-dark">
       <h1>
-        <a href="index.html"><i className="fas fa-code"></i> Recept</a>
+        <Link to='/'>Recept</Link>
+             
       </h1>
       <ul>
-        <li><a href="index.html">Home</a></li>
-        <li><a href="register.html">Register</a></li>
-        <li><a href="login.html">Login</a></li>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/register'>Register</Link></li>
+        <li><Link to='login'>Login</Link></li>
       </ul>
     </nav>
     )
